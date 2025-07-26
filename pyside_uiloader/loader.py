8cf4f31db172852309237fea524c1ed2ed351363
CrinitusFeles <crinitusfeles@gmail.com>
2025-07-27 03:56:15 +0700

    fixed ui loader

--- pyside_uiloader/loader.py
+++ pyside_uiloader/loader.py
@@ -10,13 +10,40 @@ def save_attr(root_widget: QtCore.QObject, object_name: str,
               obj: QtCore.QObject,
               object_parent: QtCore.QObject):
     setattr(root_widget, object_name, obj)
-    # print(f'"{object_name}": {type(obj)} {object_parent}')
+    print(f'"{object_name}": {type(obj)} {object_parent}')
     # return {object_parent.objectName(): object_name}
 
 
 CONTAINERS = (
-    QtWidgets.QTabWidget | QtWidgets.QStackedWidget
+    QtWidgets.QTabWidget | QtWidgets.QStackedWidget | QtWidgets.QSplitter
 )
+
+
+def extract_from_container(parent: QtCore.QObject, container: CONTAINERS):
+    if isinstance(container, QtWidgets.QTabWidget):
+        count = container.tabBar().count()
+    else:
+        count = container.count()
+    for i in range(count):
+        tab: QtWidgets.QWidget = container.widget(i)
+        save_attr(parent, tab.objectName(), tab, tab.parent())
+        tab_layout: QtWidgets.QLayout | None = tab.layout()
+        if tab_layout:
+            extract_widgets(parent, tab_layout)
+        else:
+            children = tab.children()
+            for child in children:
+                if isinstance(child, CONTAINERS):
+                    extract_from_container(parent, child)
+                elif isinstance(child, QtWidgets.QLayout):
+                    extract_widgets(parent, child)
+                elif isinstance(child, QtWidgets.QWidget):
+                    save_attr(parent, child.objectName(), child, child.parent())
+                    child_layout: QtWidgets.QLayout | None = child.layout()
+                    if child_layout:
+                        extract_widgets(parent, child_layout)
+                else:
+                    save_attr(parent, child.objectName(), child, child.parent())
 
 
 def extract_widgets(parent: QtCore.QObject, layout: QtWidgets.QLayout) -> None:
@@ -36,16 +63,7 @@ def extract_widgets(parent: QtCore.QObject, layout: QtWidgets.QLayout) -> None:
                 if container_layout:
                     extract_widgets(parent, container_layout)
             elif isinstance(widget, CONTAINERS):
-                if isinstance(widget, QtWidgets.QTabWidget):
-                    count = widget.tabBar().count()
-                else:
-                    count = widget.count()
-                for i in range(count):
-                    tab: QtWidgets.QWidget = widget.widget(i)
-                    save_attr(parent, tab.objectName(), tab, tab.parent())
-                    tab_layout: QtWidgets.QLayout | None = tab.layout()
-                    if tab_layout:
-                        extract_widgets(parent, tab_layout)
+                extract_from_container(parent, widget)
             elif widget_layout:
                 extract_widgets(parent, widget_layout)
             else:
